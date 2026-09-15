2f643ca7ccac65ea885eb8a3e7216d8c96807aa9
2453370 Trinh Vo Minh Tuan <tuan.trinh256@hcmut.edu.vn>
2026-09-15 11:24:58 +0700

    Upload submission ex_06 source

--- Submission_source/Lab_01/Ex_06.c
+++ Submission_source/Lab_01/Ex_06.c
@@ -1,102 +1,78 @@
-int counter_traffic_1 = 0, counter_traffic_2 = 0;
+/* USER CODE BEGIN PV */
+uint16_t LED[] = {
+    0x0010,  // PA4
+    0x0020,  // PA5
+    0x0040,  // PA6
+    0x0080,  // PA7
+    0x0100,  // PA8
+    0x0200,  // PA9
+    0x0400,  // PA10
+    0x0800,  // PA11
+    0x1000,  // PA12
+    0x2000,  // PA13
+    0x4000,  // PA14
+    0x8000   // PA15
+};
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  /* USER CODE BEGIN 2 */
+  int counter = 0;
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
   while (1)
   {
-      /* =====================================================
-         Traffic Light 2 = GREEN
-         Traffic Light 1 = RED
-         ===================================================== */
+    /* USER CODE END WHILE */
+	  HAL_GPIO_WritePin(GPIOA, LED[counter], GPIO_PIN_RESET);
+	  counter++;
+	  if (counter == 12)
+		  counter = 0;
 
-      // Light 1 = RED
-      HAL_GPIO_WritePin(RED_1_GPIO_Port, RED_1_Pin, GPIO_PIN_SET);
-      HAL_GPIO_WritePin(YELLOW_1_GPIO_Port, YELLOW_1_Pin, GPIO_PIN_RESET);
-      HAL_GPIO_WritePin(GREEN_1_GPIO_Port, GREEN_1_Pin, GPIO_PIN_RESET);
+	  HAL_GPIO_WritePin(GPIOA, LED[counter], GPIO_PIN_SET);
+	  HAL_Delay(100);
 
-      // Light 2 = GREEN
-      HAL_GPIO_WritePin(RED_2_GPIO_Port, RED_2_Pin, GPIO_PIN_RESET);
-      HAL_GPIO_WritePin(YELLOW_2_GPIO_Port, YELLOW_2_Pin, GPIO_PIN_RESET);
-      HAL_GPIO_WritePin(GREEN_2_GPIO_Port, GREEN_2_Pin, GPIO_PIN_SET);
-
-      // Countdown
-      counter_traffic_1 = 5, counter_traffic_2 = 3;
-      SEG7_Display(GPIOA, SEG7_1, --counter_traffic_1);
-      SEG7_Display(GPIOB, SEG7_2, --counter_traffic_2);
-      HAL_Delay(1000);
-
-      SEG7_Display(GPIOA, SEG7_1, --counter_traffic_1);
-      SEG7_Display(GPIOB, SEG7_2, --counter_traffic_2);
-      HAL_Delay(1000);
-
-      SEG7_Display(GPIOA, SEG7_1, --counter_traffic_1);
-      SEG7_Display(GPIOB, SEG7_2, --counter_traffic_2);
-      HAL_Delay(1000);
-
-
-      /* =====================================================
-         Traffic Light 2 = YELLOW
-         Traffic Light 1 = RED
-         ===================================================== */
-
-      HAL_GPIO_WritePin(RED_2_GPIO_Port, RED_2_Pin, GPIO_PIN_RESET);
-      HAL_GPIO_WritePin(YELLOW_2_GPIO_Port, YELLOW_2_Pin, GPIO_PIN_SET);
-      HAL_GPIO_WritePin(GREEN_2_GPIO_Port, GREEN_2_Pin, GPIO_PIN_RESET);
-
-      counter_traffic_2 = 2;
-      SEG7_Display(GPIOA, SEG7_1, --counter_traffic_1);
-      SEG7_Display(GPIOB, SEG7_2, --counter_traffic_2);
-      HAL_Delay(1000);
-
-      SEG7_Display(GPIOA, SEG7_1, --counter_traffic_1);
-      SEG7_Display(GPIOB, SEG7_2, --counter_traffic_2);
-      HAL_Delay(1000);
-
-
-      /* =====================================================
-         Traffic Light 1 = GREEN
-         Traffic Light 2 = RED
-         ===================================================== */
-
-      // Light 2 = RED
-      HAL_GPIO_WritePin(RED_2_GPIO_Port, RED_2_Pin, GPIO_PIN_SET);
-      HAL_GPIO_WritePin(YELLOW_2_GPIO_Port, YELLOW_2_Pin, GPIO_PIN_RESET);
-      HAL_GPIO_WritePin(GREEN_2_GPIO_Port, GREEN_2_Pin, GPIO_PIN_RESET);
-
-      // Light 1 = GREEN
-      HAL_GPIO_WritePin(RED_1_GPIO_Port, RED_1_Pin, GPIO_PIN_RESET);
-      HAL_GPIO_WritePin(YELLOW_1_GPIO_Port, YELLOW_1_Pin, GPIO_PIN_RESET);
-      HAL_GPIO_WritePin(GREEN_1_GPIO_Port, GREEN_1_Pin, GPIO_PIN_SET);
-
-      // Countdown
-      counter_traffic_1 = 3, counter_traffic_2 = 5;
-      SEG7_Display(GPIOA, SEG7_1, --counter_traffic_1);
-      SEG7_Display(GPIOB, SEG7_2, --counter_traffic_2);
-      HAL_Delay(1000);
-
-      SEG7_Display(GPIOA, SEG7_1, --counter_traffic_1);
-      SEG7_Display(GPIOB, SEG7_2, --counter_traffic_2);
-      HAL_Delay(1000);
-
-      SEG7_Display(GPIOA, SEG7_1, --counter_traffic_1);
-      SEG7_Display(GPIOB, SEG7_2, --counter_traffic_2);
-      HAL_Delay(1000);
-
-
-      /* =====================================================
-         Traffic Light 1 = YELLOW
-         Traffic Light 2 = RED
-         Countdown: 2 -> 1
-         ===================================================== */
-
-      HAL_GPIO_WritePin(RED_1_GPIO_Port, RED_1_Pin, GPIO_PIN_RESET);
-      HAL_GPIO_WritePin(YELLOW_1_GPIO_Port, YELLOW_1_Pin, GPIO_PIN_SET);
-      HAL_GPIO_WritePin(GREEN_1_GPIO_Port, GREEN_1_Pin, GPIO_PIN_RESET);
-
-      // Countdown
-      counter_traffic_1 = 2;
-      SEG7_Display(GPIOA, SEG7_1, --counter_traffic_1);
-      SEG7_Display(GPIOB, SEG7_2, --counter_traffic_2);
-      HAL_Delay(1000);
-
-      SEG7_Display(GPIOA, SEG7_1, --counter_traffic_1);
-      SEG7_Display(GPIOB, SEG7_2, --counter_traffic_2);
-      HAL_Delay(1000);
+    /* USER CODE BEGIN 3 */
   }
+  /* USER CODE END 3 */
+}
